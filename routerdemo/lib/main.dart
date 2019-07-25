9f92e3ce7c33c1382ce7c6befae01c0a4aa99a10
LemonTree-shine <18815288453@163.com>
2019-07-25 19:33:10 +0800

    路由页面跳转，传参数处理

--- routerdemo/lib/main.dart
+++ routerdemo/lib/main.dart
@@ -8,38 +8,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: FirstPage()
+      home: FirstPage(
+        aaa: "传了的话，就不会用默认值啦",
+      )
     );
   }
 }
 
 class FirstPage extends StatelessWidget {
-  const FirstPage({Key key}) : super(key: key);
-
+  final String myname = "my name is chenze";
+  String aaa;
+  String bbb = "22222";
+  FirstPage({
+    Key key,
+    this.aaa="默认值"
+  }) : super(key: key);
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child:RaisedButton(
+          child:Text("点击前往商品页面$aaa"),
+          onPressed: (){
+            //Navigator.push(context, Ma)
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) => SecondPage(
+                myname: myname,
+              )
+            ));
+          },
         ),
-        body: Center(
-          child:RaisedButton(
-            child:Text("点击前往商品页面"),
-            onPressed: (){
-              //Navigator.push(context, Ma)
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => SecondPage()
-              ));
-            },
-          ),
-        ),
-      );
+      ),
+    );
   }
 }
 
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key key}) : super(key: key);
+  String myname;
+
+  SecondPage({Key key,this.myname}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -49,9 +63,14 @@ class SecondPage extends StatelessWidget {
       ),
       body: Center(
         child: RaisedButton(
-          child: Text("返回按钮"),
+          child: Text(myname),
           onPressed: (){
-            Navigator.pop(context);
+            //Navigator.pop(context);
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context)=>FirstPage(
+                aaa:"2222222"
+              )
+            ));
           },
         ),
       ),
