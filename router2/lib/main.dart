c944d5bdf724918c7794cbb7e59cd90e3bc8dc7c
陈泽 <18815288453@163.com>
2019-07-31 19:46:20 +0800

    flutter列表渲染

--- router2/lib/main.dart
+++ router2/lib/main.dart
@@ -8,16 +8,88 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      home: ProductList(
+        products:List.generate(20, (i){
+          return Product('商品${i}', '这是一个商品${i}');
+        })
+      )
+    );
+  }
+}
+
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+
+  ProductList({Key key,@required this.products}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("商品列表"),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              title: Text('${products[index].title},${products[index].describe}'),
+              onTap: (){
+                print(index);
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return ProductDetail(
+                      productd: '${products[index].title},${products[index].describe}',
+                      productObj:products[index]
+                    );
+                  }
+                ));
+              },
+            );
+          },
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      ),
+    );
+  }
+}
+
+class ProductDetail extends StatelessWidget {
+  String productd;
+  Product productObj;
+  ProductDetail({Key key,
+    this.productd = "默认的商品详情内容",
+    this.productObj
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("商品详情"),
+      ),
+      body: Center(
+        child: Container(
+          child: RaisedButton(
+            child: Text(
+              productd,
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+            color: Colors.blue,
+            onPressed: (){
+              Navigator.pop(context);
+            },
           ),
         ),
       ),
     );
   }
+}
+
+
+class Product {
+  final String title;
+  final String describe;
+  Product(this.title,this.describe);
 }
